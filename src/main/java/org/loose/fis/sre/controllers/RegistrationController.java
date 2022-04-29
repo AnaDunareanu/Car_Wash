7da9afb196c01b46f489e99e84d7bd6dab806039
ruxandragherga <ruxandra.gherga@student.upt.ro>
2022-04-29 21:56:42 +0300

    Login account

--- src/main/java/org/loose/fis/sre/controllers/RegistrationController.java
+++ src/main/java/org/loose/fis/sre/controllers/RegistrationController.java
@@ -5,6 +5,7 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import org.loose.fis.sre.exceptions.InvalidCredentialsException;
 import org.loose.fis.sre.exceptions.UsernameAlreadyExistsException;
 import org.loose.fis.sre.services.UserService;
 
@@ -13,9 +14,17 @@ public class RegistrationController {
     @FXML
     private Text registrationMessage;
     @FXML
+    private Text loginMessage;
+    @FXML
+    private PasswordField passwordFieldLogin;
+    @FXML
+    private TextField usernameFieldLogin;
+    @FXML
     private PasswordField passwordField;
     @FXML
     private TextField usernameField;
+    @FXML
+    private TextField emailField;
     @FXML
     private ChoiceBox role;
 
@@ -27,10 +36,20 @@ public class RegistrationController {
     @FXML
     public void handleRegisterAction() {
         try {
-            UserService.addUser(usernameField.getText(), passwordField.getText(), (String) role.getValue());
+            UserService.addUser(usernameField.getText(), passwordField.getText(), emailField.getText(), (String) role.getValue());
             registrationMessage.setText("Account created successfully!");
         } catch (UsernameAlreadyExistsException e) {
             registrationMessage.setText(e.getMessage());
+        }
+    }
+
+    @FXML
+    public void handleLoginAction() {
+        try {
+            UserService.verifyUser(usernameFieldLogin.getText(), passwordFieldLogin.getText());
+            loginMessage.setText("Logged in successfully!");
+        } catch (InvalidCredentialsException e) {
+            loginMessage.setText(e.getMessage());
         }
     }
 }
