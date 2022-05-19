352972096e7c72cc1bac6f1f57716f7e1ad8c16c
ruxandragherga <ruxandra.gherga@student.upt.ro>
2022-05-19 20:42:28 +0300

    SA-5 Retrieve the list of washing types in Car Wash Details Page

--- src/main/java/org/loose/fis/sre/controllers/CarwashDetailsController.java
+++ src/main/java/org/loose/fis/sre/controllers/CarwashDetailsController.java
@@ -5,10 +5,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+
+import static org.loose.fis.sre.services.WashTypeService.getSelectedWashTypes;
+import static org.loose.fis.sre.services.WashTypeService.getSelectedWashTypesNum;
 
 public class CarwashDetailsController {
 
@@ -16,7 +20,8 @@ public class CarwashDetailsController {
     private Text carwashName;
     @FXML
     private Text address;
-
+    @FXML
+    private ListView<String> WashTypeList;
     private String loggedUser;
     public void setLoggedUser(String text) {
         loggedUser = text;
@@ -28,6 +33,28 @@ public class CarwashDetailsController {
     @FXML
     public void setAddress(String text) {
         address.setText(text);
+    }
+    @FXML
+    public void initialize()
+    {
+        if(getSelectedWashTypesNum()>0) {
+            for(String wash :  getSelectedWashTypes())
+                WashTypeList.getItems().add(wash);
+        }
+    }
+    @FXML
+    public void handleCreateAppointmentAction(javafx.event.ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("appointmentDetails.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        AppointmentDetailsController controller = loader.getController();
+        controller.setLoggedUser(loggedUser);
+        controller.setSelectedCarwash(carwashName.getText());
+        controller.setAddress(address.getText());
+        Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        appStage.setTitle("Appointment Details");
+        appStage.setScene(scene);
+        appStage.show();
     }
     @FXML
     public void handleBackAction(javafx.event.ActionEvent event) throws IOException {
